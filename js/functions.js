ffce51f369044c4c8ca2ac7636b76662bfb577f8
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-03-01 14:10:56 +0100

    Add logic

--- js/functions.js
+++ js/functions.js
@@ -9,6 +9,31 @@ function clearMessages(){
 }
 
 let computerMove = 'kamień';
-let playerMove = 'papier';
+
+
+let roundNumber = Math.floor(Math.random()* 3 + 1);
+console.log('Wylosowana liczba to '+roundNumber);
+
+
+if(roundNumber == 1){
+	printMessage('Kamień');
+} else if (roundNumber==2){
+    printMessage('Nozyce')
+} else {
+    printMessage('Papier')
+} 
+let playerInput = prompt ('Wybierz swój ruch! 1: kamień, 2: papier, 3:nozyce.');
+console.log ('Gracz wspisał: ' + playerInput);
+let playerMove= 'nieznany ruch';
+
+if (playerInput =='1'){
+    playerMove= 'Kamień';
+} else if (playerInput=='2'){
+    playerMove= 'Nozyce';
+} else if ( playerInput=='3'){
+    playerMove= 'Papier';
+} 
+printMessage ('Twój ruch to: '+playerMove);
 
 printMessage ('Zagrełem ' + computerMove +'! Jeśli Twój ruch to '+ playerMove +', to wygrywasz!');
+
